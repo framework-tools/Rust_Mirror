7c271017317d220f3bf62492076ee5c1cd1452e0
Lenard-0 <lenard@insanemarketing.com.au>
2022-11-09 15:54:30 +1030

    more testing

--- src/frontend_interface/mod.rs
+++ src/frontend_interface/mod.rs
@@ -1,12 +1,14 @@
 use std::str::FromStr;
 
+use serde_json::json;
+
 use crate::{steps_generator::{StepError, event::Event, selection::Selection}, blocks::BlockMap};
 
 pub fn execute_event() -> String { //json: String, selection_json: String, block_map_json: String
     // let rust_json = serde_json::Value::from_str(&json);
     // let rust_json = match rust_json {
     //     Ok(rust_json) => rust_json,
-    //     Err(_) => return Err(StepError("json argument should be valid json".to_string()))
+    //     Err(_) => return javascript_return_json("", Some("json argument should be valid json"))
     // };
     // let event_json = rust_json.get("event");
     // let event_json = match event_json {
@@ -24,7 +26,14 @@ pub fn execute_event() -> String { //json: String, selection_json: String, block
     //     Err(_) => return Err(StepError("Block Map json could not be parsed".to_string()))
     // };
 
-    //let object_id = bson::oid::ObjectId::new();
+    let object_id = bson::oid::ObjectId::new();
 
     return "object_id".to_string()
+}
+
+pub fn javascript_return_json(data: &str, err: Option<&str>) -> String {
+    return match err {
+        Some(err) => json!({ "data": "", "error": err }).to_string(),
+        None => json!({ "data": data, "error": "" }).to_string()
+    }
 }
