d7887c08a97cf7f5d460063efaa8c0f8bf74b21c
Lenard-0 <lenard@insanemarketing.com.au>
2022-11-09 13:17:57 +1030

    finished removal refactoring for selection from json

--- src/frontend_interface/mod.rs
+++ src/frontend_interface/mod.rs
@@ -2,7 +2,7 @@ use std::str::FromStr;
 
 use crate::steps_generator::{StepError, event::Event, selection::Selection};
 
-pub fn execute_event(json: String) -> Result<String, StepError> {
+pub fn execute_event(json: String, selection_json: String) -> Result<String, StepError> {
     let rust_json = serde_json::Value::from_str(&json);
     let rust_json = match rust_json {
         Ok(rust_json) => rust_json,
@@ -13,11 +13,6 @@ pub fn execute_event(json: String) -> Result<String, StepError> {
         Some(event_json) => event_json,
         None => return Err(StepError("json should contain 'event' field".to_string()))
     };
-    let selection_json = rust_json.get("selection");
-    let selection_json = match selection_json {
-        Some(selection_json) => selection_json,
-        None => return Err(StepError("json should contain 'selection' field".to_string()))
-    };
     let block_map_json = rust_json.get("block_map");
     let block_map_json = match block_map_json {
         Some(block_map_json) => block_map_json,
@@ -25,7 +20,7 @@ pub fn execute_event(json: String) -> Result<String, StepError> {
     };
 
     let event = Event::from_json(event_json)?;
-    let selection: Selection = match serde_json::from_str(&selection_json.to_string()) {
+    let selection: Selection = match serde_json::from_str(&selection_json) {
         Ok(selection) => selection,
         Err(_) => return Err(StepError("Selection json could not be parsed".to_string()))
     };
