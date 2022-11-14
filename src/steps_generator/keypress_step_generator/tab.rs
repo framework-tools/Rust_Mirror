f1280f6706036a057c48bca5b5004d8e6812efe5
yifan117 <yifan.qiu17@gmail.com>
2022-11-14 14:57:26 +1030

    completed should_not_create_parent_if_parent_is_root_block

--- src/steps_generator/keypress_step_generator/tab.rs
+++ src/steps_generator/keypress_step_generator/tab.rs
@@ -1,6 +1,5 @@
-use std::fs::Metadata;
 
-use crate::{step::{Step, TurnToChild, TurnToParent}, steps_generator::{StepError, selection::SubSelection, event::{KeyPressMetadata, KeyPress}}, blocks::{BlockMap, Block}};
+use crate::{step::{Step, TurnToChild, TurnToParent}, steps_generator::{StepError, selection::SubSelection, event::{KeyPressMetadata, KeyPress}}, blocks::{BlockMap, Block, RootBlock}};
 
 
 pub fn generate_steps_for_tab(block_map: &BlockMap, from: SubSelection, to: SubSelection, key_press: KeyPressMetadata) -> Result<Vec<Step>, StepError> {
@@ -10,11 +9,16 @@ pub fn generate_steps_for_tab(block_map: &BlockMap, from: SubSelection, to: SubS
         Block::InlineBlock(inline_block) => {
         
             let parent_block = inline_block.get_parent(block_map)?;
+            let parents_parent_as_root = block_map.get_root_block(&parent_block.parent);
 
             if key_press.shift_down {
+                if parents_parent_as_root.is_ok() {
+                    return Ok(vec![])
+                } else {
                 return Ok(vec![
                     Step::TurnToParent(TurnToParent { block_id: inline_block.parent })
                 ])
+                }
             } else {
                 if parent_block.index(block_map)? == 0 {
                     return Ok(vec![])
@@ -24,10 +28,6 @@ pub fn generate_steps_for_tab(block_map: &BlockMap, from: SubSelection, to: SubS
                     ])        
                 }
             }
-
-
-
-
         },
 
         Block::StandardBlock(_) => unimplemented!(),
