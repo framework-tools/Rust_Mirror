50012c879e0cca94ebb4092f0d755e1cc39f2ddd
yifan117 <yifan.qiu17@gmail.com>
2022-11-14 14:22:05 +1030

    completed should_not_create_child_block_as_first_index

--- src/steps_generator/keypress_step_generator/tab.rs
+++ src/steps_generator/keypress_step_generator/tab.rs
@@ -4,9 +4,20 @@ use crate::{step::{Step, TurnToChild}, steps_generator::{StepError, selection::S
 pub fn generate_steps_for_tab(block_map: &BlockMap, from: SubSelection, to: SubSelection) -> Result<Vec<Step>, StepError> {
     let from_block = block_map.get_block(&from.block_id)?;
     match from_block {
-        Block::InlineBlock(inline_block) => return Ok(vec![
-            Step::TurnToChild(TurnToChild { block_id: inline_block.parent })
-        ]),
+
+        Block::InlineBlock(inline_block) => {
+        
+            let parent_block = inline_block.get_parent(block_map)?;
+
+            if parent_block.index(block_map)? == 0 {
+                return Ok(vec![])
+            } else {
+                return Ok(vec![
+                    Step::TurnToChild(TurnToChild { block_id: inline_block.parent })
+                ])        
+            }
+        },
+
         Block::StandardBlock(_) => unimplemented!(),
         Block::Root(_) => unimplemented!(),
     }
