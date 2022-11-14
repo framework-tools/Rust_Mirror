65c7c37b543f3175498d9c25b8fed4c6a5a83d41
Lenard-0 <lenard@insanemarketing.com.au>
2022-11-14 14:27:57 +1030

    added test for turn to parent steps

--- tests/children_steps_tests.rs
+++ tests/children_steps_tests.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod tests {
-    use rust_mirror::{new_ids::NewIds, blocks::{RootBlock, BlockMap}, steps_generator::{event::{Event, KeyPress, Key}, selection::{SubSelection, Selection}, generate_steps}, step::Step};
+    use rust_mirror::{new_ids::NewIds, blocks::{RootBlock, BlockMap}, steps_generator::{event::{Event, KeyPress, Key, KeyPressMetadata}, selection::{SubSelection, Selection}, generate_steps}, step::Step};
     use serde_json::json;
 
     #[test]
@@ -138,5 +138,77 @@ mod tests {
         let steps = generate_steps(&event, &block_map, selection).unwrap();
 
         assert_eq!(steps.len(), 0);
+    }
+
+    #[test]
+    fn shift_tab_on_inline_should_generate_turn_to_parent_step() {
+        let mut new_ids = NewIds::hardcoded_new_ids_for_tests();
+
+        let root_block_id = new_ids.get_id().unwrap();
+        let paragraph_block_id1 = new_ids.get_id().unwrap();
+        let paragraph_block_id2 = new_ids.get_id().unwrap();
+        let inline_block_id1 = new_ids.get_id().unwrap();
+        let inline_block_id2 = new_ids.get_id().unwrap();
+
+        let inline_block1 = json!({
+            "_id": inline_block_id1.clone(),
+            "kind": "inline",
+            "_type": "text",
+            "content": {
+                "text": "Hello"
+            },
+            "marks": [],
+            "parent": paragraph_block_id1.clone()
+        });
+        let paragraph_block1 = json!({
+            "_id": paragraph_block_id1.clone(),
+            "kind": "standard",
+            "_type": "paragraph",
+            "content": {
+                "inline_blocks": [inline_block_id1.clone()]
+            },
+            "children": [paragraph_block_id2.clone()],
+            "marks": [],
+            "parent": root_block_id.clone()
+        });
+        let inline_block2 = json!({
+            "_id": inline_block_id2.clone(),
+            "kind": "inline",
+            "_type": "text",
+            "content": {
+                "text": "Goodbye"
+            },
+            "marks": [],
+            "parent": paragraph_block_id2.clone()
+        });
+        let paragraph_block2 = json!({
+            "_id": paragraph_block_id2.clone(),
+            "kind": "standard",
+            "_type": "paragraph",
+            "content": {
+                "inline_blocks": [inline_block_id2.clone()]
+            },
+            "children": [],
+            "marks": [],
+            "parent": paragraph_block_id1.clone()
+        });
+
+        let root_block = RootBlock::json_from(root_block_id, vec![paragraph_block_id1.clone()]);
+        let block_map = BlockMap::from(vec![
+            inline_block1.to_string(), inline_block2.to_string(), paragraph_block1.to_string(), paragraph_block2.to_string(), root_block.to_string()
+        ]).unwrap();
+        let event = Event::KeyPress(KeyPress { key: Key::Tab, metadata: KeyPressMetadata { shift_down: true, meta_down: false, ctrl_down: false, alt_down: false } });
+        let sub_selection = SubSelection::from(inline_block_id2.clone(), 4, None);
+        let selection = Selection::from(sub_selection.clone(), sub_selection.clone());
+
+        let steps = generate_steps(&event, &block_map, selection).unwrap();
+
+        assert_eq!(steps.len(), 1);
+        match &steps[0] {
+            Step::TurnToParent(turn_to_parent_step) => {
+                assert_eq!(turn_to_parent_step.block_id, paragraph_block_id2);
+            },
+            _ => panic!("Expected ReplaceStep")
+        }
     }
 }
