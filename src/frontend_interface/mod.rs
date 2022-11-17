1ec64e7365005c6fdca4c4f51ca9ca6c9ed0665a
Lenard-0 <lenard@insanemarketing.com.au>
2022-11-17 17:01:22 +1030

    testing

--- src/frontend_interface/mod.rs
+++ src/frontend_interface/mod.rs
@@ -13,7 +13,7 @@ pub fn execute_event(
     //let block_map = BlockMap::from_js_map(block_map_js);
     let selection = Selection::from_js_obj(selection_js).unwrap();
     let event = Event::from_js_obj(event_js).unwrap();
-    //let mut new_ids = NewIds::Js(new_ids_arr);
+    let mut new_ids = NewIds::Js(new_ids_arr);
 
     return Response { selection: None, err: None }
     // let steps = match generate_steps(&event, &block_map, selection) {
