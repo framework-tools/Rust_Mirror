1d0663df129373765d14e59132865b89c2120233
Lenard-0 <lenard@insanemarketing.com.au>
2023-01-05 17:01:43 +1030

    fixed bug in cut and paste steps generation

--- src/steps_generator/clipboard_steps.rs
+++ src/steps_generator/clipboard_steps.rs
@@ -4,15 +4,23 @@ use super::{selection::SubSelection, StepError, keypress_step_generator::backspa
 
 
 pub fn generate_cut_steps(from: SubSelection, to: SubSelection, block_map: &BlockMap) -> Result<Vec<Step>, StepError> {
-    return Ok(vec![
-        vec![Step::Copy(from.clone(), to.clone())],
-        generate_steps_for_backspace(block_map, from, to)?,
-    ].into_iter().flatten().collect())
+    if from == to {
+        return Ok(vec![Step::Copy(from.clone(), to.clone())])
+    } else {
+        return Ok(vec![
+            vec![Step::Copy(from.clone(), to.clone())],
+            generate_steps_for_backspace(block_map, from, to)?,
+        ].into_iter().flatten().collect())
+    }
 }
 
 pub fn generate_paste_steps(from: SubSelection, to: SubSelection, block_map: &BlockMap) -> Result<Vec<Step>, StepError> {
-    return Ok(vec![
-        generate_steps_for_backspace(block_map, from.clone(), to.clone())?,
-        vec![Step::Paste(from, to)],
-    ].into_iter().flatten().collect())
+    if from == to {
+        return Ok(vec![Step::Paste(from, to)])
+    } else {
+        return Ok(vec![
+            generate_steps_for_backspace(block_map, from.clone(), to.clone())?,
+            vec![Step::Paste(from, to)],
+        ].into_iter().flatten().collect())
+    }
 }
