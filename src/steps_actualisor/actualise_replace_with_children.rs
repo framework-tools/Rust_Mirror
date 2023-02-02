fb3fc25b44d18d05fd26ba58500edc7cfe8d4f35
Lenard-0 <lenard@insanemarketing.com.au>
2023-02-02 16:50:35 +1030

    testing

--- src/steps_actualisor/actualise_replace_with_children.rs
+++ src/steps_actualisor/actualise_replace_with_children.rs
@@ -7,17 +7,17 @@ pub fn actualise_replace_with_children(
     mut block_map: BlockMap,
     mut blocks_to_update: Vec<String>,
 ) -> Result<UpdatedState, StepError> {
-    let block = block_map.get_standard_block(&replace_with_children_event.block_id)?;
-    let parent = block.get_parent(&block_map)?;
-    let block_index = block.index(&block_map)?;
+    // let block = block_map.get_standard_block(&replace_with_children_event.block_id)?;
+    // let parent = block.get_parent(&block_map)?;
+    // let block_index = block.index(&block_map)?;
 
-    update_state_tools::splice_children(
-        parent,
-        block_index..block_index + 1,
-        block.children,
-        &mut blocks_to_update,
-        &mut block_map
-    )?;
+    // update_state_tools::splice_children(
+    //     parent,
+    //     block_index..block_index + 1,
+    //     block.children,
+    //     &mut blocks_to_update,
+    //     &mut block_map
+    // )?;
 
     return Ok(UpdatedState {
         block_map,
