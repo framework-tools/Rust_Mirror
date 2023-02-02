48d34541537113fbfb8e87547fc6b9eb4d07b3bf
Lenard-0 <lenard@insanemarketing.com.au>
2023-02-02 14:58:37 +1030

    added some tests to check if errors were frontend or backend issue when dragging childremn

--- tests/drag_and_drop_tests.rs
+++ tests/drag_and_drop_tests.rs
@@ -78,6 +78,180 @@ mod tests {
         let updated_root_block = updated_state.block_map.get_root_block(&root_block_id).unwrap();
         assert_eq!(updated_root_block.children, vec![paragraph_block_id2.clone(), paragraph_block_id1.clone()]);
     }
+    #[test]
+    fn can_drop_block_below_child_to_child_within_same_parent() {
+        let mut new_ids = NewIds::hardcoded_new_ids_for_tests();
+
+        let root_block_id = new_ids.get_id().unwrap();
+        let paragraph_block_id1 = "1".to_string();
+        let paragraph_block_id2 = "2".to_string();
+        let paragraph_block_id3 = "3".to_string();
+        let inline_block_id1 = new_ids.get_id().unwrap();
+        let inline_block_id2 = new_ids.get_id().unwrap();
+
+        let inline_block1 = json!({
+            "_id": inline_block_id1.clone(),
+            "kind": "inline",
+            "_type": "text",
+            "content": {
+                "text": "Hello"
+            },
+            "marks": [],
+            "parent": paragraph_block_id1.clone()
+        });
+        let paragraph_block1 = json!({
+            "_id": paragraph_block_id1.clone(),
+            "kind": "standard",
+            "_type": "paragraph",
+            "content": {
+                "inline_blocks": [inline_block_id1.clone()]
+            },
+            "children": [paragraph_block_id2.clone(), paragraph_block_id3.clone()],
+            "marks": [],
+            "parent": root_block_id.clone()
+        });
+        let inline_block2 = json!({
+            "_id": inline_block_id2.clone(),
+            "kind": "inline",
+            "_type": "text",
+            "content": {
+                "text": "Goodbye"
+            },
+            "marks": [],
+            "parent": paragraph_block_id2.clone()
+        });
+        let paragraph_block2 = json!({
+            "_id": paragraph_block_id2.clone(),
+            "kind": "standard",
+            "_type": "paragraph",
+            "content": {
+                "inline_blocks": [inline_block_id2.clone()]
+            },
+            "children": [],
+            "marks": [],
+            "parent": paragraph_block_id1.clone()
+        });
+        let paragraph_block3 = json!({
+            "_id": paragraph_block_id3.clone(),
+            "kind": "standard",
+            "_type": "paragraph",
+            "content": {
+                "inline_blocks": [inline_block_id2.clone()]
+            },
+            "children": [],
+            "marks": [],
+            "parent": paragraph_block_id1.clone()
+        });
+
+        let root_block = RootBlock::json_from(root_block_id.clone(), vec![paragraph_block_id1.clone()]);
+        let block_map = BlockMap::from(vec![
+            inline_block1.to_string(), inline_block2.to_string(), paragraph_block1.to_string(), paragraph_block2.to_string(),
+            paragraph_block3.to_string(), root_block.to_string()
+        ]).unwrap();
+        let event = Event::DropBlock(DropBlockEvent {
+            drag_block_id: paragraph_block_id2.clone(),
+            drop_block_id: paragraph_block_id3.clone(),
+            side_dropped: Side::Bottom
+        });
+        // selection does not matter for this event
+        let sub_selection = SubSelection::from(inline_block_id2.clone(), 4, None);
+        let selection = Selection::from(sub_selection.clone(), sub_selection.clone());
+
+        let steps = generate_steps(&event, &block_map, selection).unwrap();
+        let updated_state = actualise_steps(steps, block_map, &mut new_ids, CustomCopy::new()).unwrap();
+
+        let updated_root_block = updated_state.block_map.get_root_block(&root_block_id).unwrap();
+        assert_eq!(updated_root_block.children, vec![paragraph_block_id1.clone()]);
+        let updated_paragraph_block1 = updated_state.block_map.get_standard_block(&paragraph_block_id1).unwrap();
+        assert_eq!(updated_paragraph_block1.children, vec![paragraph_block_id3.clone(), paragraph_block_id2.clone()]);
+    }
+    #[test]
+    fn can_drop_block_from_root_to_a_child_within_same_parent() {
+        let mut new_ids = NewIds::hardcoded_new_ids_for_tests();
+
+        let root_block_id = new_ids.get_id().unwrap();
+        let paragraph_block_id1 = "1".to_string();
+        let paragraph_block_id2 = "2".to_string();
+        let paragraph_block_id3 = "3".to_string();
+        let inline_block_id1 = new_ids.get_id().unwrap();
+        let inline_block_id2 = new_ids.get_id().unwrap();
+
+        let inline_block1 = json!({
+            "_id": inline_block_id1.clone(),
+            "kind": "inline",
+            "_type": "text",
+            "content": {
+                "text": "Hello"
+            },
+            "marks": [],
+            "parent": paragraph_block_id1.clone()
+        });
+        let paragraph_block1 = json!({
+            "_id": paragraph_block_id1.clone(),
+            "kind": "standard",
+            "_type": "paragraph",
+            "content": {
+                "inline_blocks": [inline_block_id1.clone()]
+            },
+            "children": [paragraph_block_id2.clone()],
+            "marks": [],
+            "parent": root_block_id.clone()
+        });
+        let inline_block2 = json!({
+            "_id": inline_block_id2.clone(),
+            "kind": "inline",
+            "_type": "text",
+            "content": {
+                "text": "Goodbye"
+            },
+            "marks": [],
+            "parent": paragraph_block_id2.clone()
+        });
+        let paragraph_block2 = json!({
+            "_id": paragraph_block_id2.clone(),
+            "kind": "standard",
+            "_type": "paragraph",
+            "content": {
+                "inline_blocks": [inline_block_id2.clone()]
+            },
+            "children": [],
+            "marks": [],
+            "parent": paragraph_block_id1.clone()
+        });
+        let paragraph_block3 = json!({
+            "_id": paragraph_block_id3.clone(),
+            "kind": "standard",
+            "_type": "paragraph",
+            "content": {
+                "inline_blocks": [inline_block_id2.clone()]
+            },
+            "children": [],
+            "marks": [],
+            "parent": root_block_id.clone()
+        });
+
+        let root_block = RootBlock::json_from(root_block_id.clone(), vec![paragraph_block_id1.clone(), paragraph_block_id3.clone()]);
+        let block_map = BlockMap::from(vec![
+            inline_block1.to_string(), inline_block2.to_string(), paragraph_block1.to_string(), paragraph_block2.to_string(),
+            paragraph_block3.to_string(), root_block.to_string()
+        ]).unwrap();
+        let event = Event::DropBlock(DropBlockEvent {
+            drag_block_id: paragraph_block_id3.clone(),
+            drop_block_id: paragraph_block_id2.clone(),
+            side_dropped: Side::Top
+        });
+        // selection does not matter for this event
+        let sub_selection = SubSelection::from(inline_block_id2.clone(), 4, None);
+        let selection = Selection::from(sub_selection.clone(), sub_selection.clone());
+
+        let steps = generate_steps(&event, &block_map, selection).unwrap();
+        let updated_state = actualise_steps(steps, block_map, &mut new_ids, CustomCopy::new()).unwrap();
+
+        let updated_root_block = updated_state.block_map.get_root_block(&root_block_id).unwrap();
+        assert_eq!(updated_root_block.children, vec![paragraph_block_id1.clone()]);
+        let updated_paragraph_block1 = updated_state.block_map.get_standard_block(&paragraph_block_id1).unwrap();
+        assert_eq!(updated_paragraph_block1.children, vec![paragraph_block_id3.clone(), paragraph_block_id2.clone()]);
+    }
 
     #[test]
     fn can_drop_block_on_side_with_no_layout_block() {
