de8da15c6efa389c36edb4be349157eb5fb97a93
Lenard-0 <lenard@insanemarketing.com.au>
2023-03-02 13:59:25 +1030

    added test for layout block editor

--- tests/layout_block_content_editing_tests.rs
+++ tests/layout_block_content_editing_tests.rs
@@ -5,7 +5,245 @@ mod tests {
 
     /// <LayoutBlock> <Column><p1/><p2/></Column> <Column><p1/><p2/></Column> </LayoutBlock>
     #[test]
-    fn can_handle_editing_across_2_layout_columns() {
+    fn can_handle_editing_across_2_layout_columns_with_3_layout_columns() {
+        let mut new_ids = NewIds::hardcoded_new_ids_for_tests();
+
+        let inline_block_id1 = "Inline1".to_string();
+        let inline_block_id2 = "Inline2".to_string();
+        let inline_block_id3 = "Inline3".to_string();
+        let inline_block_id4 = "Inline4".to_string();
+        let inline_block_id5 = "Inline5".to_string();
+        let inline_block_id6 = "Inline6".to_string();
+        let paragraph_block_id1 = "Paragraph1".to_string();
+        let paragraph_block_id2 = "Paragraph2".to_string();
+        let paragraph_block_id3 = "Paragraph3".to_string();
+        let paragraph_block_id4 = "Paragraph4".to_string();
+        let paragraph_block_id5 = "Paragraph5".to_string();
+        let paragraph_block_id6 = "Paragraph6".to_string();
+        let layout_block_id = "layout".to_string();
+        let layout_column_id1 = "column1".to_string();
+        let layout_column_id2 = "column2".to_string();
+        let layout_column_id3 = "column3".to_string();
+        let root_block_id = "Root".to_string();
+
+        let inline_block1 = json!({
+            "_id": inline_block_id1.clone(),
+            "kind": "inline",
+            "_type": "text",
+            "content": {
+                "text": "hello"
+            },
+            "marks": [],
+            "parent": paragraph_block_id1.clone()
+        }).to_string();
+        let inline_block2 = json!({
+            "_id": inline_block_id2.clone(),
+            "kind": "inline",
+            "_type": "text",
+            "content": {
+                "text": "b"
+            },
+            "marks": [],
+            "parent": paragraph_block_id2.clone()
+        }).to_string();
+        let inline_block3 = json!({
+            "_id": inline_block_id3.clone(),
+            "kind": "inline",
+            "_type": "text",
+            "content": {
+                "text": "goodbye"
+            },
+            "marks": [],
+            "parent": paragraph_block_id3.clone()
+        }).to_string();
+        let inline_block4 = json!({
+            "_id": inline_block_id4.clone(),
+            "kind": "inline",
+            "_type": "text",
+            "content": {
+                "text": "goodbye"
+            },
+            "marks": [],
+            "parent": paragraph_block_id4.clone()
+        }).to_string();
+        let inline_block5 = json!({
+            "_id": inline_block_id5.clone(),
+            "kind": "inline",
+            "_type": "text",
+            "content": {
+                "text": "goodbye"
+            },
+            "marks": [],
+            "parent": paragraph_block_id5.clone()
+        }).to_string();
+        let inline_block6 = json!({
+            "_id": inline_block_id6.clone(),
+            "kind": "inline",
+            "_type": "text",
+            "content": {
+                "text": "goodbye"
+            },
+            "marks": [],
+            "parent": paragraph_block_id6.clone()
+        }).to_string();
+        let paragraph1 = json!({
+            "_id": paragraph_block_id1.clone(),
+            "kind": "standard",
+            "_type": "paragraph",
+            "content": {
+                "inline_blocks": [inline_block_id1.clone()]
+            },
+            "children": [],
+            "marks": [],
+            "parent": layout_column_id1.clone()
+        }).to_string();
+        let paragraph2 = json!({
+            "_id": paragraph_block_id2.clone(),
+            "kind": "standard",
+            "_type": "paragraph",
+            "content": {
+                "inline_blocks": [inline_block_id2.clone()]
+            },
+            "children": [],
+            "marks": [],
+            "parent": layout_column_id1.clone()
+        }).to_string();
+        let paragraph3 = json!({
+            "_id": paragraph_block_id3.clone(),
+            "kind": "standard",
+            "_type": "paragraph",
+            "content": {
+                "inline_blocks": [inline_block_id3.clone()]
+            },
+            "children": [],
+            "marks": [],
+            "parent": layout_column_id2.clone()
+        }).to_string();
+        let paragraph4 = json!({
+            "_id": paragraph_block_id4.clone(),
+            "kind": "standard",
+            "_type": "paragraph",
+            "content": {
+                "inline_blocks": [inline_block_id4.clone()]
+            },
+            "children": [],
+            "marks": [],
+            "parent": layout_column_id2.clone()
+        }).to_string();
+        let paragraph5 = json!({
+            "_id": paragraph_block_id5.clone(),
+            "kind": "standard",
+            "_type": "paragraph",
+            "content": {
+                "inline_blocks": [inline_block_id5.clone()]
+            },
+            "children": [],
+            "marks": [],
+            "parent": layout_column_id3.clone()
+        }).to_string();
+        let paragraph6 = json!({
+            "_id": paragraph_block_id6.clone(),
+            "kind": "standard",
+            "_type": "paragraph",
+            "content": {
+                "inline_blocks": [inline_block_id6.clone()]
+            },
+            "children": [],
+            "marks": [],
+            "parent": layout_column_id3.clone()
+        }).to_string();
+        let layout_block = json!({
+            "_id": layout_block_id,
+            "kind": "standard",
+            "_type": "layout",
+            "content": {
+                "horizontal": true
+            },
+            "children": [layout_column_id1.clone(), layout_column_id2.clone()],
+            "marks": [],
+            "parent": root_block_id.clone().to_string()
+        }).to_string();
+        let layout_column1 = json!({
+            "_id": layout_column_id1.clone(),
+            "kind": "standard",
+            "_type": "layout",
+            "content": {
+                "horizontal": false
+            },
+            "children": [paragraph_block_id1.clone(), paragraph_block_id2.clone()],
+            "marks": [],
+            "parent": layout_block_id.clone()
+        }).to_string();
+        let layout_column2 = json!({
+            "_id": layout_column_id2.clone(),
+            "kind": "standard",
+            "_type": "layout",
+            "content": {
+                "horizontal": false
+            },
+            "children": [paragraph_block_id3.clone(), paragraph_block_id4.clone()],
+            "marks": [],
+            "parent": layout_block_id.clone()
+        }).to_string();
+        let layout_column3 = json!({
+            "_id": layout_column_id3.clone(),
+            "kind": "standard",
+            "_type": "layout",
+            "content": {
+                "horizontal": false
+            },
+            "children": [paragraph_block_id5.clone(), paragraph_block_id6.clone()],
+            "marks": [],
+            "parent": layout_block_id.clone()
+        }).to_string();
+
+        let root_block = RootBlock::json_from(root_block_id.clone(), vec![layout_block_id.clone()]).to_string();
+
+        let block_map = BlockMap::from(vec![
+            inline_block1, inline_block2, inline_block3, inline_block4, inline_block5, inline_block6,
+            paragraph1, paragraph2, paragraph3, paragraph4, paragraph5, paragraph6,
+            layout_block, layout_column1, layout_column2, layout_column3, root_block,
+        ]).unwrap();
+        let event = Event::KeyPress(KeyPress::new(Key::Backspace, None));
+        let anchor = SubSelection {
+            block_id: layout_column_id1.clone(),
+            offset: 0,
+            subselection: Some(Box::new(SubSelection {
+                block_id: paragraph_block_id1.clone(),
+                offset: 0,
+                subselection: Some(Box::new(SubSelection { block_id: inline_block_id1.clone(), offset: 2, subselection: None })),
+            }))
+        };
+        let head = SubSelection {
+            block_id: layout_column_id2.clone(),
+            offset: 0,
+            subselection: Some(Box::new(SubSelection {
+                block_id: paragraph_block_id3.clone(),
+                offset: 0,
+                subselection: Some(Box::new(SubSelection { block_id: inline_block_id3.clone(), offset: 2, subselection: None })),
+            }))
+        };
+
+        let selection = Selection::from(anchor, head);
+
+        let steps = generate_steps(&event, &block_map, selection).unwrap();
+        let updated_state = actualise_steps(steps, block_map, &mut new_ids, CustomCopy::new()).unwrap();
+
+        let updated_column1 = updated_state.block_map.get_standard_block(&layout_column_id1).unwrap();
+        assert_eq!(updated_column1.children, vec![paragraph_block_id1.clone(), paragraph_block_id4.clone()]);
+
+        let updated_inline1 = updated_state.block_map.get_inline_block(&inline_block_id1).unwrap();
+        assert_eq!(updated_inline1.text().unwrap().clone().to_string(), "heodbye".to_string());
+
+        let updated_column2 = updated_state.block_map.get_standard_block(&layout_column_id2).unwrap();
+        assert_eq!(updated_column2.children.len(), 0);
+        let updated_column3 = updated_state.block_map.get_standard_block(&layout_column_id3).unwrap();
+        assert_eq!(updated_column3.children.len(), 2);
+    }
+
+    /// <LayoutBlock> <Column><p1/><p2/></Column> <Column><p1/><p2/></Column> </LayoutBlock>
+    #[test]
+    fn can_handle_editing_across_2_layout_columns_with_only_2_layout_columns() {
         let mut new_ids = NewIds::hardcoded_new_ids_for_tests();
 
         let inline_block_id1 = "Inline1".to_string();
