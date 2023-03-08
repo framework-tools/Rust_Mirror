f2ff138933f8ebdd2d18b1329fc93e8b00fbdcb1
Lenard-0 <lenard@insanemarketing.com.au>
2023-03-09 10:15:28 +1030

    added bug fix for enter with list block

--- src/steps_generator/keypress_step_generator/enter.rs
+++ src/steps_generator/keypress_step_generator/enter.rs
@@ -11,22 +11,22 @@ pub fn generate_steps_for_enter(block_map: &BlockMap, from: SubSelection, to: Su
         for step in delete_selected_steps {
             steps.push(step);
         }
-    } else if caret_is_at_start_of_block(&from, &to, block_map)? {
-        let inline_block = block_map.get_inline_block(&from.block_id)?;
-        let std_block = inline_block.get_parent(block_map)?;
-        let parent = std_block.get_parent(block_map)?;
-        return Ok(vec![
-            Step::AddBlock(AddBlockStep {
-                block_id: parent.id(),
-                child_offset: std_block.index(block_map)?,
-                block_type: StandardBlockType::Paragraph(ContentBlock::new(vec![])),
-                focus_block_below: true
-            })
-        ])
-    } else {
+    }  else {
         let std_block = block_map.get_inline_block(&from.block_id)?.get_parent(block_map)?;
         if std_block.is_list() && std_block.text_is_empty(block_map)? {
             return turn_into_paragraph_step(std_block.id())
+        } else if caret_is_at_start_of_block(&from, &to, block_map)? {
+            let inline_block = block_map.get_inline_block(&from.block_id)?;
+            let std_block = inline_block.get_parent(block_map)?;
+            let parent = std_block.get_parent(block_map)?;
+            return Ok(vec![
+                Step::AddBlock(AddBlockStep {
+                    block_id: parent.id(),
+                    child_offset: std_block.index(block_map)?,
+                    block_type: StandardBlockType::Paragraph(ContentBlock::new(vec![])),
+                    focus_block_below: true
+                })
+            ])
         }
     }
 
