92db71ed39d0ec8d6d2a3b28d3ed6c41592157b4
Lenard-0 <lenard@insanemarketing.com.au>
2023-03-22 13:01:41 +1030

    testing duplicate

--- src/steps_actualisor/actualise_duplicate.rs
+++ src/steps_actualisor/actualise_duplicate.rs
@@ -9,20 +9,20 @@ pub fn actualise_duplicate(
     mut blocks_to_update: Vec<String>,
     new_ids: &mut NewIds
 ) -> Result<UpdatedState, StepError> {
-    let block = block_map.get_standard_block(&block_id)?;
-    let parent = block.get_parent(&block_map)?;
-    let original_block_index = block.index(&block_map)?;
-    let mut top_block = vec![block];
-    let block_and_all_descendants = get_all_blocks(&top_block, &block_map)?;
-    reassign_ids(block_and_all_descendants, &mut top_block, &mut block_map, new_ids, &mut blocks_to_update)?;
+    // let block = block_map.get_standard_block(&block_id)?;
+    // let parent = block.get_parent(&block_map)?;
+    // let original_block_index = block.index(&block_map)?;
+    // let mut top_block = vec![block];
+    // let block_and_all_descendants = get_all_blocks(&top_block, &block_map)?;
+    // reassign_ids(block_and_all_descendants, &mut top_block, &mut block_map, new_ids, &mut blocks_to_update)?;
 
-    update_state_tools::splice_children(
-        parent,
-        original_block_index+1..original_block_index+1,
-        vec![top_block[0].id()],
-        &mut blocks_to_update,
-        &mut block_map
-    )?;
+    // update_state_tools::splice_children(
+    //     parent,
+    //     original_block_index+1..original_block_index+1,
+    //     vec![top_block[0].id()],
+    //     &mut blocks_to_update,
+    //     &mut block_map
+    // )?;
 
     return Ok(UpdatedState {
         block_map,
