93df073b2d62129270dba7c4af2a361d6dd5b3c4
Lenard-0 <lenard@insanemarketing.com.au>
2023-04-17 14:56:13 +0930

    made fix for turn into step where doesn't have content block

--- src/steps_actualisor/actualise_turn_into.rs
+++ src/steps_actualisor/actualise_turn_into.rs
@@ -2,25 +2,25 @@ use crate::{step::TurnInto, blocks::{BlockMap, Block, standard_blocks::StandardB
 
 use super::UpdatedState;
 
-// This function takes in a 
-// - TurnInto step, 
-// - a BlockMap, 
-// - a vector of Strings representing the blocks to update. 
-//It returns a Result object with an UpdatedState as the Ok variant, 
+// This function takes in a
+// - TurnInto step,
+// - a BlockMap,
+// - a vector of Strings representing the blocks to update.
+//It returns a Result object with an UpdatedState as the Ok variant,
 //or a StepError as the Err variant.
 
-// The function first retrieves the standard block identified by the block_id field of the TurnInto step. 
-//Then, it uses the update_block_content method of the new_block_type field 
+// The function first retrieves the standard block identified by the block_id field of the TurnInto step.
+//Then, it uses the update_block_content method of the new_block_type field
 //of the TurnInto step to update the content of the retrieved block with the new block type.
 
-// Next, the function updates the block map with the updated block 
-//and updates the list of blocks to update. 
-//It then creates a subselection representing the end of the updated block 
-//and returns an UpdatedState object with 
-// - the updated block map, 
-// - the created subselection as the selection, 
-// - the updated list of blocks to update, 
-// - an empty list of blocks to remove, 
+// Next, the function updates the block map with the updated block
+//and updates the list of blocks to update.
+//It then creates a subselection representing the end of the updated block
+//and returns an UpdatedState object with
+// - the updated block map,
+// - the created subselection as the selection,
+// - the updated list of blocks to update,
+// - an empty list of blocks to remove,
 // -  a None value for the copy field.
 pub fn actualise_turn_into_step(
     turn_into_step: TurnInto,
@@ -28,7 +28,10 @@ pub fn actualise_turn_into_step(
     mut blocks_to_update: Vec<String>
 ) -> Result<UpdatedState, StepError> {
     let block = block_map.get_standard_block(&turn_into_step.block_id)?;
-    let new_block_content = turn_into_step.new_block_type.update_block_content(block.content_block()?.clone())?;
+    let new_block_content = match block.content_block() {
+        Ok(content_block) => turn_into_step.new_block_type.update_block_content(content_block.clone())?,
+        Err(_) => turn_into_step.new_block_type.clone()
+    };
     let block = StandardBlock {
         _id: block._id,
         content: new_block_content,
