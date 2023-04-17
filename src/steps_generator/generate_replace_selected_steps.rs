a4a4194b98118520079f427e7b7aad89da343bc7
Lenard-0 <lenard@insanemarketing.com.au>
2023-04-17 14:48:58 +0930

    added edge case for deleting single block with no content that is selected

--- src/steps_generator/generate_replace_selected_steps.rs
+++ src/steps_generator/generate_replace_selected_steps.rs
@@ -1,4 +1,4 @@
-use crate::{blocks::{BlockMap, Block}, step::{Step, ReplaceStep, ReplaceSlice}};
+use crate::{blocks::{BlockMap, Block, standard_blocks::{StandardBlockType, content_block::ContentBlock}}, step::{Step, ReplaceStep, ReplaceSlice, TurnInto}};
 
 use super::{selection::SubSelection, StepError};
 
@@ -17,14 +17,25 @@ pub fn generate_replace_selected_steps(
                 slice: ReplaceSlice::String(replace_with)
             })
         ]),
-        Block::StandardBlock(standard_block) => Ok(vec![
-            Step::ReplaceStep(ReplaceStep {
-                block_id: standard_block.parent,
-                from,
-                to,
-                slice: ReplaceSlice::String(replace_with)
-            })
-        ]),
+        Block::StandardBlock(standard_block) => {
+            if &from.block_id == &to.block_id {
+                return Ok(vec![
+                    Step::TurnInto(TurnInto {
+                        block_id: from.block_id,
+                        new_block_type: StandardBlockType::Paragraph(ContentBlock::new(vec![]))
+                    })
+                ])
+            } else {
+                Ok(vec![
+                    Step::ReplaceStep(ReplaceStep {
+                        block_id: standard_block.parent,
+                        from,
+                        to,
+                        slice: ReplaceSlice::String(replace_with)
+                    })
+                ])
+            }
+        },
         Block::Root(_) => return Err(StepError("Cannot perform replace step on root".to_string()))
     }
 }
