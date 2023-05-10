688e559ac028a9554ebb6b46e24c0aea0fadcb4b
Silenle <tonychihungho@gmail.com>
2023-05-10 15:59:08 +0930

    changed actualise_mirror_step to only do one step

--- src/backend_interface/mod.rs
+++ src/backend_interface/mod.rs
@@ -4,21 +4,20 @@ use crate::{blocks::BlockMap, step::Step, steps_generator::StepError, new_ids::N
 
 
 
-pub fn actualise_mirror_steps(
-    steps_as_json: Vec<(String, String)>,
+pub fn actualise_mirror_step(
+    step_as_json: (String, String),
     new_ids: Vec<String>,
     block_map_rust: HashMap<String, String>
 ) -> Result<HashMap<String, String>, StepError> {
     let mut block_map = BlockMap::Rust(block_map_rust);
     let mut new_ids = NewIds::Rust(new_ids);
 
-    for (_type, data) in steps_as_json {
-        if &_type != "copy" {
-            let step = Step::from_json(&_type, &data)?;
-            let updated_state = 
-                actualise_steps(vec![step.clone()], block_map, &mut new_ids, CustomCopy::new())?;
-            block_map = updated_state.block_map;
-        }
+    let (_type, data) = step_as_json;
+    if &_type != "copy" {
+        let step = Step::from_json(&_type, &data)?;
+        let updated_state = 
+            actualise_steps(vec![step.clone()], block_map, &mut new_ids, CustomCopy::new())?;
+        block_map = updated_state.block_map;
     }
     return match block_map {
         BlockMap::Rust(block_map) => Ok(block_map),
